1dd70deec9c9d8a89337c072900706ffe542469d
Phelipe Soares Simeao <pssimeao@stefanini.com>
2019-04-12 14:07:45 -0300

    tudo

--- Teste-Api/src/app.js
+++ Teste-Api/src/app.js
@@ -7,6 +7,9 @@ const router = express.Router();
 //Rotas
 const index = require('./routes/index');
 const personRoute = require('./routes/personRoute');
+
+
+app.use(express.json());
 app.use('/', index);
 app.use('/persons', personRoute);
 module.exports = app;
--- Teste-Api/src/routes/index.js
+++ Teste-Api/src/routes/index.js
@@ -8,4 +8,13 @@ router.get('/', function (req, res, next) {
         version: "0.0.1"
     });
 });
+
+router.post('/postado', (req, res) => {
+    //JSON.stringify(req.body);
+    console.log(req.params);
+    console.log(req.body.login);
+    res.status(200).send(`hello, ${req.body.login}, ${req.header('Content-Type')}`);
+});
+
+
 module.exports = router;
