172c4bea0d25833f206ad07cff5dc8d70e1c6b52
Phelipe Soares Simeao <pssimeao@stefanini.com>
2019-04-16 11:22:07 -0300

    buscando funcionarios e computadores

--- Teste-Api/src/app.js
+++ Teste-Api/src/app.js
@@ -8,11 +8,13 @@ const router = express.Router();
 const index = require('./routes/index');
 const personRoute = require('./routes/personRoute');
 const dev = require('./routes/desenvolvedor');
+const gest = require('./routes/gestor');
 
 
 app.use(express.json());
 app.use('/', index);
 app.use('/persons', personRoute);
 app.use('/dev', dev);
+app.use('/gestor', gest);
 
 module.exports = app;
--- Teste-Api/src/routes/gestor.js
+++ Teste-Api/src/routes/gestor.js
@@ -3,12 +3,120 @@ const router = express.Router();
 const Database = require('../../Database');
 const config = require('../../config');
 
+router.post('/login', (req, res) => {
+    const login = req.body.loginJSON;
+    const senha = req.body.senhaJSON;
+    console.log(login);
+    if (login == undefined || login == "" || senha == undefined || senha == "") {
+        console.log("Login e senha não podem estar vazios!!");
+        return false;
+    }
+    else {
+        console.log("Sucesso!!");
+    }
+    Database.query(`Select * from TB_FUNCIONARIO where nmEmail = '${login}' `).then(resultadoQuery => {
+        const linhas = resultadoQuery.recordsets[0];
+        console.log(linhas);
+        res.status(200).send(resultadoQuery);
+        //console.log(resultadoQuery.recordset[0].nmSenha); // método 1 para capturar dado
+        //console.log(linhas[0].nmSenha); //método 2 para capturar dado
+        //console.log(valido);
+        const senhaBanco = linhas[0].nmSenha;
+        const valido = senha === senhaBanco ? true : false;
+        console.log(valido);
+
+    })
+
+
+    console.log(login);
+    console.log(senha);
+});
+
 router.post('/cadastro', (req, res) => {
 
+    //dados para cadastro do funcionario
+
+    const IDEMPRESA = req.body.idEmpresa;
+    const IDGESTOR = req.body.idGestor;
+    const NMFUNCIONARIO = req.body.nmFuncionario;
+    const NMEMAIL = req.body.email;
+    const NMCARGO = req.body.cargo;
+    const NMSENHA = req.body.senha;
+
+    console.log(IDEMPRESA);
+    console.log(IDGESTOR);
+    console.log(NMFUNCIONARIO);
+    console.log(NMEMAIL);
+    console.log(NMCARGO);
+    console.log(NMSENHA);
+
+    if ( //IDEMPRESA == "" || IDEMPRESA == undefined || 
+        //IDGESTOR == null || IDGESTOR == undefined || 
+        NMFUNCIONARIO == "" || NMFUNCIONARIO == undefined ||
+        NMEMAIL == "" || NMEMAIL == undefined ||
+        NMCARGO == "" || NMCARGO == undefined ||
+        NMSENHA == "" || NMSENHA == undefined) {
+        console.log('Dados incorretos, preencher corretamente!');
+        return false;
+    }
+
+    criarUser(null, NMFUNCIONARIO, NMEMAIL, NMCARGO, NMSENHA);
 });
 
-router.post('/login', (req, res) =>{
+router.get('/apis', (res, req) =>{
+
+    getFuncionariosPCs(18).then((resultado) =>{
+        console.log(resultado);
+    });
 
 });
+
+function getFuncionariosPCs(IDGESTOR){
+    let string = `SELECT * FROM TB_FUNCIONARIO
+                     INNER JOIN TB_COMPUTADOR ON TB_FUNCIONARIO.IDFUNCIONARIO = TB_COMPUTADOR.IDFUNCIONARIO
+                        INNER JOIN TB_LEITURA_PC ON TB_COMPUTADOR.IDCOMPUTADOR = TB_LEITURA_PC.IDCOMPUTADOR WHERE IDGESTOR = ${IDGESTOR}`;
+    return new Promise((resolve, reject) => {
+        Database.query(string).then(resultado => {
+            //console.log(resultado.recordset);
+            resolve(resultado.recordset);
+        }).catch(erro => {
+            console.log(erro);
+        });
+    });
+}
+
+function existeUsuario(emailFuncionario) {
+    let querystring = `SELECT * FROM TB_FUNCIONARIO WHERE NMEMAIL = '${emailFuncionario}'`;
+    return new Promise((resolve, reject) => {
+        Database.query(querystring).then(results => {
+            const existe = results.rowsAffected > 0 ? true : false;
+            resolve(existe);
+        }).catch(error => {
+            reject(error);
+        });
+    });
+}
+
+function criarUser(IDEMPRESA, NMFUNCIONARIO, NMEMAIL, NMCARGO, NMSENHA) {
+    return new Promise((resolve, reject) => {
+        existeUsuario(NMEMAIL).then(existe => {
+            if (!existe) {
+                //INSERT INTO TB_USUARIO(IDEMPRESA, IDGESTOR, NMFUNCIONARIO, NMEMAIL, NMCARGO, NMSENHA
+                Database.query(`INSERT INTO TB_FUNCIONARIO(NMFUNCIONARIO, NMEMAIL, NMCARGO, NMSENHA)
+                    VALUES ('${NMFUNCIONARIO}', '${NMEMAIL}', '${NMCARGO}', '${NMSENHA}')`).then(results => {
+                    resolve(results);
+                }).catch(error => {
+                    reject(error);
+                });
+            } else {
+                console.log(`Usuario com o email ${NMEMAIL} já cadastrado.`);
+            }
+        }).catch((error) => {
+            reject(error);
+        });
+    });
+}
+
+
 
 module.exports = router;
