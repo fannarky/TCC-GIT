c8b56dd30e4bec7c431081e6ca26e055f978c01a
Phelipe Soares Simeao <pssimeao@stefanini.com>
2019-04-26 12:14:27 -0300

    aparecer e desaparecer feito xD

--- projeto-scss/src/app/app.module.ts
+++ projeto-scss/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
